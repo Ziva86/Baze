7cbc31380ae7a9904533ececc4273f404e1210d1
Ziva86 <>
2019-01-16 13:10:23 +0100

    ApexSQL Data Diff synchronization

---
+++ Pubs/Tables/StaticData/dbo.pub_info [Data].sql
@@ -0,0 +1,13 @@
+﻿-- =======================================================
+-- Synchronization script for table: [dbo].[pub_info]
+-- =======================================================
+Print 'Synchronization script for table: [dbo].[pub_info]'
+
+INSERT INTO [dbo].[pub_info] ([pub_id]) VALUES ('0736')
+INSERT INTO [dbo].[pub_info] ([pub_id]) VALUES ('0877')
+INSERT INTO [dbo].[pub_info] ([pub_id]) VALUES ('1389')
+INSERT INTO [dbo].[pub_info] ([pub_id]) VALUES ('1622')
+INSERT INTO [dbo].[pub_info] ([pub_id]) VALUES ('1756')
+INSERT INTO [dbo].[pub_info] ([pub_id]) VALUES ('9901')
+INSERT INTO [dbo].[pub_info] ([pub_id]) VALUES ('9952')
+INSERT INTO [dbo].[pub_info] ([pub_id]) VALUES ('9999')
