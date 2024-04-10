4c5046a4fabddb3616170670bfb7dd6647de7e16
Marko <mareziva@gmail.com>
2024-04-10 16:01:31 +0200

    Delete NANA directory

--- NANA/DataTypes/dbo.Names.sql
+++
@@ -1,5 +0,0 @@
-CREATE TYPE [dbo].[Names]
-AS TABLE (
-		[Name]     [varchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-)
-GO
--- NANA/Mara/Triggers/dbo.trgAfterInsert.sql
+++
@@ -1,9 +0,0 @@
-SET ANSI_NULLS ON
-SET QUOTED_IDENTIFIER ON
-GO
--- New Trigger
-CREATE TRIGGER trgAfterInsert ON [dbo].[UserAddress]
-FOR INSERT
-AS
-PRINT 'Data entered successfully'
-GO
